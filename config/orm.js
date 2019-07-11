6566091da1823c04a497e35e87d637082aefc46d
Rey Lim <Reylim15@gmail.com>
2019-07-11 15:25:01 -0400

    Deleted some stuff, Gotta add comments later :D

--- config/orm.js
+++ config/orm.js
@@ -17,17 +17,16 @@ function printQuestionMarks(num) {
 function objToSql(ob) {
 	var arr = [];
 
-	// loop through the keys and push the key/value as a string int arr
+	
 	for (var key in ob) {
 		var value = ob[key];
-		// check to skip hidden properties
+	
 		if (Object.hasOwnProperty.call(ob, key)) {
-			// if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+		
 			if (typeof value === 'string' && value.indexOf(' ') >= 0) {
 				value = "'" + value + "'";
 			}
-			// e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-			// e.g. {sleepy: true} => ["sleepy=true"]
+		
 			arr.push(key + '=' + value);
 		}
 	}
@@ -61,7 +60,7 @@ function objToSql(ob) {
 				cb(result);
 			});
 		},
-		// An example of objColVals would be {name: panther, sleepy: true}
+	
 		updateONE: function(table, objColVals, condition, cb) {
 			var queryString = 'UPDATE ' + table;
 
