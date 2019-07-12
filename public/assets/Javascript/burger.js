4cd47a14b91622529ebecb6066cc909671799d10
Rey Lim <Reylim15@gmail.com>
2019-07-12 07:32:28 -0400

    Push

--- public/assets/Javascript/burger.js
+++ public/assets/Javascript/burger.js
@@ -1,14 +1,12 @@
 $(function() {
     $(".create-form").on("submit", function(event) {
       event.preventDefault();
-  
       var newBurger = {
         burger_name: $("#newburger")
           .val()
           .trim(),
         devoured: 0
       };
-  
       $.ajax("/api/burgers", {
         type: "POST",
         data: newBurger
@@ -17,10 +15,8 @@ $(function() {
         location.reload();
       });
     });
-  
     $(".eatburger").on("click", function(event) {
       event.preventDefault();
-  
       var id = $(this).data("id");
       var devouredState = {
         devoured: 1
@@ -30,20 +26,18 @@ $(function() {
         type: "PUT",
         data: devouredState
       }).then(function() {
-        console.log("Burger devoured");
+        console.log("burger ate");
         location.reload();
       });
     });
   
     $(".trashburger").on("click", function(event) {
-      event.preventDefault();
-  
-      var id = $(this).data("id");
-  
-      // Send the DELETE request.
+      event.preventDefault(); 
+      var id = $(this).data("id"); 
+    
       $.ajax({
         type: "DELETE",
-        url: "/api/burgers/" + id
-      }).then(location.reload());
+        url: "/api/burgers/" + id 
+         }).then(location.reload());
     });
   });
