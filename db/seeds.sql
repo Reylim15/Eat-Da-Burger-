a57d150a96b38e8eea1893274bea96eade5471f1
Rey Lim <Reylim15@gmail.com>
2019-07-12 05:39:32 -0400

    Almost Done with backend

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ('burger1', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('burger2', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('burger1', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('burger2', true);
 INSERT INTO burgers (burger_name, devoured) VALUES ('burger3', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('burger4', false);
 INSERT INTO burgers (burger_name, devoured) VALUES ('burger5', false);
-INSERT INTO burgers (burger_name, devoured) VALUES ('burger6', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('burger6', true);
--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
-var routes = require("./controllers/burger_Controller.jss");
+var routes = require("./controllers/burger_Controller.js");
 
 app.use(routes);
 
