6c3793da1cfd2ff7feb3252602435f618c199d8a
Rey Lim <Reylim15@gmail.com>
2019-07-12 07:07:29 -0400

    Updated Burger Controller

--- controllers/burger_Controller.js
+++ controllers/burger_Controller.js
@@ -7,52 +7,53 @@ var burger = require("../model/burger.js");
 // ROUTES YEETYEET FUN FUN
 
 // Home sweet HOME yall
-router.get("/", function(req, res) {
-    burger.selectALL(function(data) {
-      var hbsObject = {
-        burgers: data
-      };
-      // console.log(hbsObject);
-      res.render("index", hbsObject);
-    });
+router.get("/", function (req, res) {
+  burger.selectALL(function (data) {
+    var hbsObject = {
+      burgers: data
+    };
+    // console.log(hbsObject);
+    res.render("index", hbsObject);
   });
-  
-  router.post("/burgers", function(req, res) {
-    burger.insertONE([
-      "burger_name"
-    ], [
-      req.body.burger_name,
-    ], function(data) {
-      res.redirect("/");
-    });
-  });
-  
-  router.put("/burgers/:id", function(req, res) {
-    var condition = "id = " + req.params.id;
-  
-    // console.log("condition", condition);
-  
-    burger.updateONE({
-      devoured:true
-    }, condition, function(data) {
-    res.redirect("/");
-    
-    });
-  });
-  
-// YOUR TIME WILL COME TO DELETE THE BURGERS POLLUTING MY DB !, But now is not the time little one.
 
-//   router.delete("/api/cats/:id", function(req, res) {
-//     var condition = "id = " + req.params.id;
-  
-//     cat.delete(condition, function(result) {
-//       if (result.affectedRows == 0) {
-//         // If no rows were changed, then the ID must not exist, so 404
-//         return res.status(404).end();
-//       } else {
-//         res.status(200).end();
-//       }
-//     });
-//   });
+
+router.post("/burgers", function (req, res) {
+  burger.insertONE([
+    "burger_name", "devoured"
+  ], [
+      req.body.burger_name, req.body.devoured
+    ], function (result) {
+      res.json({ id: result.insertID });
+    });
+});
+
+router.put("/api/burgers/:id", function (req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.updateONE({
+    devoured: req / body / devoured
+  }, condition, function (result) {
+    if (result.changedRows === 0) {
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+router.delete("/api/burgers/:id", function (req, res) {
+  var condition = "id = " + req.params.id;
+
+  burger.deleteONE(condition, function (result) {
+    if (result.changedRows === 0) {
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+});
 
 module.exports = router;
